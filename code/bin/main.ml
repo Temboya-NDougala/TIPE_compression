7f425a2d8f0ddeadb44c400a4ac9c90d8f584e14
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-01-21 22:38:54 +0100

    tester fonctions d'ecritures

--- code/bin/main.ml
+++ code/bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+open TIPE_compression
+
+let () =
+  let output_test = open_out "output_test.txt" in
+  let out = Bit_wise_channel.of_out_channel output_test in
+  Bit_wise_channel.write_n_bits 8 out (Char.code 'a');
+  Bit_wise_channel.write_n_bits 8 out (Char.code 'b');
+  Bit_wise_channel.close_out_stream out;
