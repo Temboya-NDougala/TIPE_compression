878fae068394090e997f1238a6c5d3ac20726d75
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-01-21 18:41:52 +0100

    definir interface des flux bit-a-bit

--- code/lib/bit_wise_channel.mli
+++ code/lib/bit_wise_channel.mli
@@ -1,5 +1,45 @@
-type t
+type in_stream
 (**
-    flux de donnees que l'on peut manipuler bit par bit,
+    flux de donnees entrant que l'on peut manipuler bit par bit,
     contrairement aux flux implementes dans la librairie standard,
     ou l'on maipule les donnees octet par octet.*)
+
+val of_in_channel : In_channel.t -> in_stream
+(**
+    [of_in_channel out] retourne un flux de donnees bit-a-bit a
+    partir de n'importe quel flux de donnees entrant.*)
+
+val read_bit : in_stream -> int
+(**
+    [read_bit istream] lit le premier bit de [istream].*)
+
+val read_n_bits : in_stream -> int -> int
+(**
+    [read_n_bits istream n] lit les [n] premiers bits de [istream]*)
+
+type out_stream
+(**
+    flux de donnees sortant que l'on peut manipuler bit par bit,
+    contrairement aux flux implementes dans la librairie standard,
+    ou l'on maipule les donnees octet par octet.*)
+
+val of_out_channel : Out_channel.t -> out_stream
+(**
+    [of_out_channel out] retourne un flux de donnees bit-a-bit a
+    partir de n'importe quel flux de donnees sortant.*)
+
+val write_bit : out_stream -> int -> unit
+(**
+    [write_bit out to_write] ecrits le bit de poids faible de [to_write] dans
+    le flux bit-a-bit [out].*)
+
+val write_n_bits : out_stream -> int -> int -> unit
+(**
+    [write_n_bits out n to_write] ecrits les [n] bits de poids faible de
+    [to_write] en ordre de poids croissant.
+    eg : [write_n_bits out 4 13] ecrits "1011" dans [out] et pas "1101"*)
+
+val close_out_stream : out_stream -> unit
+(**
+    [close_out_stream out] must called to close a binary stream.
+    Closing the underlying byte-wise channel instead can lead to a garbage file.*)
