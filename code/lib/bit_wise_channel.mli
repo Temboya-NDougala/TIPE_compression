72156718d27143e00c21d80386327a94505351bf
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-01-21 13:47:01 +0100

    debuter implementation des bitwise channels

---
+++ code/lib/bit_wise_channel.mli
@@ -0,0 +1,5 @@
+type t
+(**
+    flux de donnees que l'on peut manipuler bit par bit,
+    contrairement aux flux implementes dans la librairie standard,
+    ou l'on maipule les donnees octet par octet.*)
