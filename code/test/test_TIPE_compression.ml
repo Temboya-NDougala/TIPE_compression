152071de96d615ebc33237800ecc4c3985a251c9
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-05-21 19:44:08 +0200

    ameliorer tests ecriture bit-wise

--- code/test/test_TIPE_compression.ml
+++ code/test/test_TIPE_compression.ml
@@ -6,6 +6,8 @@ let () =
   Bit_wise_channel.write_n_bits 16 out (((Char.code 'a') lsl 8) + (Char.code 'b'));
   Bit_wise_channel.write_n_bits 2 out 3;
   Bit_wise_channel.close_out_stream out;
-  let input_test = open_in "output_test.txt" in
-  let _ = Bit_wise_channel.of_in_channel input_test in
-  ()
+  let output_reading = open_in "output_test.txt" in
+  assert (input_byte output_reading = Char.code 'a');
+  assert (input_byte output_reading = Char.code 'b');
+  assert (input_byte output_reading = 3);
+  assert (input_byte output_reading = 2)
