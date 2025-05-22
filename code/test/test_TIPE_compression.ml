27bfe736b265703b0564506da1bc2aeca41ac840
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-05-22 19:49:48 +0200

    modifier tests des flux bit-a-bit pour ne pas afficher les contenus des fichiers

--- code/test/test_TIPE_compression.ml
+++ code/test/test_TIPE_compression.ml
@@ -19,10 +19,11 @@ let bitwise_reading_test() =
   Bit_wise_channel.write_n_bits 4 reading_test1 2;
   Bit_wise_channel.close_out_stream reading_test1;
   let reading1 = Bit_wise_channel.of_in_channel (open_in "reading_test1.txt") in
-  for _ = 1 to 12 do
+  (* for _ = 1 to 12 do
     print_int (Bit_wise_channel.read_bit reading1);
-  done;
-  print_char '\n';
+  done; *)
+  assert (Bit_wise_channel.read_n_bits reading1 12 = "011111100010");
+  (* print_char '\n'; *)
   Bit_wise_channel.close_in_stream reading1;
   let reading_test2 = Bit_wise_channel.of_out_channel (open_out "reading_test2.txt") in
   Bit_wise_channel.write_n_bits 9 reading_test2 256;
@@ -31,8 +32,9 @@ let bitwise_reading_test() =
   (* for _ = 1 to 9 do
     print_int (Bit_wise_channel.read_bit reading2);
   done; *)
-  print_string (Bit_wise_channel.read_n_bits reading2 9);
-  print_char '\n';
+  (* print_string (Bit_wise_channel.read_n_bits reading2 9); *)
+  assert (Bit_wise_channel.read_n_bits reading2 9 = "100000000");
+  (* print_char '\n'; *)
 ;;
 
 bitwise_writing_test();
