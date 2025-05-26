9cd19fd7e7e1b2cf77489bef5aac1694b53dd2fb
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-05-26 20:32:50 +0200

    tester et debugger implementation Huffman

---
+++ code/test/huffman_test/huffman_test.ml
@@ -0,0 +1,13 @@
+let test_huffman preffix =
+  Huffman.zip (preffix^".txt");
+  print_string (preffix^".txt zipped\n");
+  Huffman.unzip (preffix^".txt.hff");
+  let archive_channel = open_in (preffix^"_archive.txt") in
+  let unzipped_channel = open_in (preffix^".txt") in
+  assert (In_channel.input_all archive_channel = In_channel.input_all unzipped_channel);
+  close_in archive_channel;
+  close_in unzipped_channel
+;;
+
+test_huffman "source";;
+test_huffman "bee_movie";;
