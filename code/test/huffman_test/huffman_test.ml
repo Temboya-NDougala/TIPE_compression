2e4d9caef64c8d2c4e9a5cd8c3114bfbb4a7ebd4
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2025-05-28 15:54:38 +0200

    organiser huffman_test

--- code/test/huffman_test/huffman_test.ml
+++ code/test/huffman_test/huffman_test.ml
@@ -1,9 +1,9 @@
 let test_huffman preffix =
-  Huffman.zip (preffix^".txt");
+  Huffman.zip (preffix^"/"^preffix^".txt");
   print_string (preffix^".txt zipped\n");
-  Huffman.unzip (preffix^".txt.hff");
-  let archive_channel = open_in (preffix^"_archive.txt") in
-  let unzipped_channel = open_in (preffix^".txt") in
+  Huffman.unzip (preffix^"/"^preffix^".txt.hff");
+  let archive_channel = open_in (preffix^"/"^preffix^"_archive.txt") in
+  let unzipped_channel = open_in (preffix^"/"^preffix^".txt") in
   assert (In_channel.input_all archive_channel = In_channel.input_all unzipped_channel);
   close_in archive_channel;
   close_in unzipped_channel
